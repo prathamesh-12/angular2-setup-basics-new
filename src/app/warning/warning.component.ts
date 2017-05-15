39424b697223e2a28fe1b31937bbcd41abea9831
Prathamesh Nimbkar <prathamesh_nimbkar@outlook.com>
2017-05-15 22:54:03 +0530

    Add files via upload

---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'warning-msg',
+    template: `
+        <div class="alert alert-warning">This is Warning Message!</div>
+    `
+})
+
+export class WarningComponent {
+
+}
